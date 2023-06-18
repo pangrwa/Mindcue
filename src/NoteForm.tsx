32b3299f884d9c74e1eacffc43f273a81ecfefab
Rusted_57 <pangrwa@gmail.com>
2023-06-18 21:27:12 +0800

    add: NoteForm structure

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,8 +1,10 @@
-import { Form, Stack, Row, Col } from "react-bootstrap";
+import { Form, Stack, Row, Col, Button } from "react-bootstrap";
 import CreatableReactSelect from "react-select/creatable";
+import { Link } from "react-router-dom";
 
 
 export function NoteForm() {
+
     return (
         <Form>
             <Stack gap={4}>
@@ -21,6 +23,20 @@ export function NoteForm() {
                     </Form.Group>
                     </Col>
                 </Row>
+
+                <Row>
+                <Form.Group controlId="markdown">
+                    <Form.Label>Body</Form.Label>
+                    <Form.Control required as="textarea" rows={15}></Form.Control>
+                </Form.Group>
+                </Row>
+
+                <Stack direction="horizontal" gap={2} className="justify-content-end">
+                    <Button type="submit" variant="primary">Save</Button>
+                    <Link to="..">
+                        <Button type="button" variant="outline-secondary">Cancel</Button>
+                    </Link>
+                </Stack>
             </Stack>
         </Form>
     )
