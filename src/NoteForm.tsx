e204cac565640c4156204e7778c6dfa405827c18
Pang Rui Wei <>
2023-06-19 22:36:09 +0800

    add: Note data, ref values for title and markdown

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,25 +1,57 @@
+import { FormEvent, useRef, useState } from "react";
 import { Form, Stack, Row, Col, Button } from "react-bootstrap";
 import CreatableReactSelect from "react-select/creatable";
 import { Link } from "react-router-dom";
+import { NoteData, Tag } from "./App";
 
 
-export function NoteForm() {
+type NoteFormProps = {
+    onSubmit: (data: NoteData) => void
+} 
+
+export function NoteForm({ onSubmit }): NoteFormProps {
+
+    const titleRef = useRef<HTMLInputElement>(null);
+    const markdownRef = useRef<HTMLTextAreaElement>(null);
+    const [selectedTags, setSelectedTags] = useState<Tag[]>([])
+
+
+    function handleSubmit(e: FormEvent) {
+        e.preventDefault();
+
+        onSubmit({
+            title: titleRef.current!.value,
+            markdown: markdownRef.current!.value,
+            tags: [],
+        })
+    }
 
     return (
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <Stack gap={4}>
                 <Row>
                     <Col>
                     <Form.Group controlId="title">
                         <Form.Label>Title</Form.Label>
-                        <Form.Control required />
+                        <Form.Control ref={titleRef} required />
                     </Form.Group>
                     </Col>
                 
                     <Col>
                     <Form.Group controlId="tags">
                         <Form.Label>Tags</Form.Label>
-                        <CreatableReactSelect isMulti />
+                        <CreatableReactSelect
+                            value={selectedTags.map(tag => {
+                                return { label: tag.label, value: tag.id }
+                            })} 
+                            /*
+                            onChange={tags => {
+                                setSelectedTags(tag => {
+                                    return { label: tag.label, id: tag.value}
+                                })
+                            }}
+                            */
+                        isMulti />
                     </Form.Group>
                     </Col>
                 </Row>
@@ -27,7 +59,7 @@ export function NoteForm() {
                 <Row>
                 <Form.Group controlId="markdown">
                     <Form.Label>Body</Form.Label>
-                    <Form.Control required as="textarea" rows={15}></Form.Control>
+                    <Form.Control ref={markdownRef} required as="textarea" rows={15}></Form.Control>
                 </Form.Group>
                 </Row>
 
