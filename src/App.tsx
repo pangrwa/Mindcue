65731cf24bcaa7c9e88c568e6d72bf22542399cc
Rusted_57 <pangrwa@gmail.com>
2023-06-20 21:43:11 +0800

    add: logic NewNote

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,16 @@ import { Container } from "react-bootstrap";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { NewNote } from "./NewNote";
 import { useLocalStorage } from "./useLocalStorage";
+import { useMemo } from "react";
+import { v4 as uuidV4 } from "uuid";
 
 export type Note = {
   id: string
-  & NoteData
-}
+} & NoteData
 
 export type RawNote = {
   id: string;
-}
+} & RawNoteData
 
 export type RawNoteData = {
   title: string;
@@ -35,11 +36,23 @@ function App() {
   const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []) // A way to keep information persistence
   const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
 
+  const notesWithTags = useMemo(() => {
+    return notes.map(note => {
+      return {...note, tags: tags.filter(tag => note.tagIds.includes(tag.id))}
+    })
+  }, [notes, tags]);
+
+  function onCreateNote({ tags, ...data }: NoteData) {
+    setNotes(prevNotes => {
+      return [...prevNotes, { ...data, id: uuidV4(), tagIds: tags.map(tag => tag.id) }]
+    })
+  }
+
   return (
     <Container className="my-4">
       <Routes>
         <Route path="/" element={<h1>Hi</h1>} />
-        <Route path="/new" element={<NewNote />} />
+        <Route path="/new" element={<NewNote onSubmit={onCreateNote} />} />
         <Route path="/:id">
           <Route index element={<h1>Show</h1>} />
           <Route path="edit" element={<h1>Edit</h1>} />
