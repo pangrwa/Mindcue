f8e85ee8bc430c6b69ba209478294188df012143
Rusted_57 <pangrwa@gmail.com>
2023-06-25 16:16:26 +0800

    add: Noteshow page

---
+++ src/NoteLayout.tsx
@@ -0,0 +1,22 @@
+import { Note } from "./App";
+import { Navigate, Outlet, useOutletContext, useParams } from "react-router-dom";
+
+
+type NotesLayoutProps = {
+    notes: Note[];
+}
+
+export function NoteLayout({ notes }: NotesLayoutProps) {
+    const { id } = useParams();
+    const note = notes.find(n => n.id === id);
+
+    if (note === null) {
+        return <Navigate to="/" replace />
+    }
+
+    return <Outlet context={note} />
+}
+
+export function useNote() {
+    return useOutletContext<Note>()
+}
--- src/useLocalStorage.ts
+++ src/useLocalStorage.ts
@@ -15,9 +15,9 @@ export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
     })
 
 
-    useEffect(() => {
-        localStorage.setItem(key, JSON.stringify(value));
-    }, [value, key])
+    useEffect(() => 
+        localStorage.setItem(key, JSON.stringify(value))
+    , [value, key])
 
     return [value, setValue] as [T, typeof setValue];
 } 
